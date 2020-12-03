00bbe6f370ab15dfc656641c008dc67bc5fbdfb0
eikenei <orm_y@yahoo.co.jp>
2020-12-03 22:41:53 +0900

    create comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-	render(){
-		return <div><h1>Hello world</h1></div>;
-	}
+//class App extends Component {
+//	render(){
+//		//return <div>Hello world!!</div>;
+//		return <h1>Hello world!!</h1>;
+//	}
+//}
+
+const App = () => {
+	return(
+		<div>
+			<Cat />
+			
+		</div>
+	)
 }
 
+const Cat = () => {
+	return <div>NNNNN</div>
+}
 
 export default App;
